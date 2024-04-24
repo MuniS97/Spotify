4792f3079536cdb181216eabae7743af596b0b30
MuniS67 <tursunovmunis34@gmail.com>
2024-04-24 14:31:52 +0500

    fixed

--- src/components/Genre.jsx
+++ src/components/Genre.jsx
@@ -3,7 +3,7 @@
 export default function Genre(props) {
     return (
         <>
-            <div className="select-none w-[360px] h-[100px] bg-[#303030] text-white flex justify-start items-center gap-5">
+            <div className="cursor-pointer select-none w-[360px] h-[100px] bg-[#303030] text-white flex justify-start items-center gap-5">
                 <img className="w-[100px] h-[100px] object-cover" src="/images/genre.png" alt="picture" />
                 <span>{props.title}</span>
             </div>
