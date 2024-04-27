856b86970fe779d2a56f596981662a391bd39e0b
MuniS67 <tursunovmunis34@gmail.com>
2024-04-27 23:48:48 +0500

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "home.jsx"),
+        library: resolve(__dirname, "pages/library.jsx"),
+        login: resolve(__dirname, "pages/login.jsx"),
+        search: resolve(__dirname, "pages/search.jsx"),
+      },
+    },
+  },
 })
